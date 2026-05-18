5050385051043ac986540decc4289761f983e4d1
gautham-sedin <gautham.r@sedintechnologies.com>
2026-05-18 11:16:36 +0530

    feat: useState implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import TodoItem from './TodoItem';
 
 function App() {
-  const todos = ["First task", "Second task", "Third task"];
+  const [todos, setTodos] = useState(["First task", "Second task", "Third task"]);
+  console.log("Current todos: ", todos);
+
+  // function handleClick() {
+  //   setTodos(["New task"]);
+  // }
 
   return (
     <>
       <h1>To do list!</h1>
+      {/* <button onClick={handleClick}>Click this button</button> */}
       <ul>
       {
         todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <TodoItem key={index} text={todo} />
         ))
       }
       </ul>
